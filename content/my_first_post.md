0cb3b7c97555b4e925073859f7ae34a9b8257ce8
Rag Sagar.V <ragsagar@gmail.com>
2012-10-10 22:13:49 +0530

    Setting up blog using pelican and adding my first post

---
+++ content/my_first_post.md
@@ -0,0 +1,9 @@
+Date: 2012-10-09
+Title: Hello World from my new blog!
+Category: Blog
+Tags: blogging, test
+
+I bought the domain ragsagar.in and I was trying to setup github pages to host
+my account. I am using pelican to generate the pages. So see you soon with some
+interesting posts :-)
+
---
+++ pelicanconf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Rag Sagar.V"
+SITENAME = u"r4g54g4r's h4ckl0g"
+SITEURL = 'ragsagar.in'
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = 'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Wordpress Blog', 'http://ragsagar.wordpress.com'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/ragsagar'),
+        ('Facebook', 'http://facebook.com/ragsagar'),
+        ('Github', 'http://github.com/ragsagar'),
+        ('Flickr', 'http://flickr.com/ragsagar'))
+
+DEFAULT_PAGINATION = 5
