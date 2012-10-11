5054827d19dc677cf99bfd2028fdc2a4474f09b4
ragsagar-mq <ragsagar@mquotient.net>
2012-10-11 17:16:45 +0530

    Changing theme again

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,6 +30,6 @@ GOOGLE_ANALYTICS = 'UA-35491289-1'
 REVERSE_ARCHIVE_ORDER = True
 
 # Theme settings
-THEME = '/home/ragsagar/Documents/pelican-themes/syte'
+THEME = '/home/ragsagar/Documents/pelican-themes/dev-random'
 #THEME_STATIC_PATHS = ['static']
 #CSS_FILE = 'main.css'
