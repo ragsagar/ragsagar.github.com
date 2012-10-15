bd9f6ace6a66aa7000a5baeb8156b3dca4e26253
ragsagar-mq <ragsagar@mquotient.net>
2012-10-15 18:25:53 +0530

    Changing the theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,6 +30,6 @@ GOOGLE_ANALYTICS = 'UA-35491289-1'
 REVERSE_ARCHIVE_ORDER = True
 
 # Theme settings
-THEME = '/home/ragsagar/Documents/pelican-themes/bootstrap'
+THEME = '/home/ragsagar/Documents/pelican-themes/notmyidea-cms'
 #THEME_STATIC_PATHS = ['static']
 #CSS_FILE = 'main.css'
